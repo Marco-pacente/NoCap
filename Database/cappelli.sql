3f42a213e82061ec0755ff6aa8aa07e5f2f885cb
Marco-pacente <66440998+Marco-pacente@users.noreply.github.com>
2023-03-11 14:33:59 +0100

    Add files via upload

---
+++ Database/cappelli.sql
@@ -0,0 +1,163 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Mar 09, 2023 alle 09:28
+-- Versione del server: 10.4.21-MariaDB
+-- Versione PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cappelli`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `cappelli`
+--
+
+CREATE TABLE `cappelli` (
+  `id_cap` int(11) NOT NULL,
+  `nome` varchar(15) NOT NULL,
+  `descrizione` varchar(50) NOT NULL,
+  `tipo` varchar(15) NOT NULL,
+  `materiale` varchar(15) NOT NULL,
+  `id_fornitore` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `carrello`
+--
+
+CREATE TABLE `carrello` (
+  `id_user` int(11) NOT NULL,
+  `id_storage` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `fornitore`
+--
+
+CREATE TABLE `fornitore` (
+  `id_fornitore` int(11) NOT NULL,
+  `nome` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `storage`
+--
+
+CREATE TABLE `storage` (
+  `id_storage` int(11) NOT NULL,
+  `id_cap` int(11) NOT NULL,
+  `taglia` varchar(6) NOT NULL,
+  `colore` varchar(15) NOT NULL,
+  `quantità` int(11) NOT NULL,
+  `prezzo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `birthdate` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `cappelli`
+--
+ALTER TABLE `cappelli`
+  ADD PRIMARY KEY (`id_cap`),
+  ADD KEY `id_fornitore` (`id_fornitore`);
+
+--
+-- Indici per le tabelle `carrello`
+--
+ALTER TABLE `carrello`
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_storage` (`id_storage`);
+
+--
+-- Indici per le tabelle `fornitore`
+--
+ALTER TABLE `fornitore`
+  ADD PRIMARY KEY (`id_fornitore`);
+
+--
+-- Indici per le tabelle `storage`
+--
+ALTER TABLE `storage`
+  ADD PRIMARY KEY (`id_storage`),
+  ADD KEY `id_cap` (`id_cap`);
+
+--
+-- Indici per le tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `cappelli`
+--
+ALTER TABLE `cappelli`
+  ADD CONSTRAINT `cappelli_ibfk_1` FOREIGN KEY (`id_fornitore`) REFERENCES `fornitore` (`id_fornitore`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `carrello`
+--
+ALTER TABLE `carrello`
+  ADD CONSTRAINT `carrello_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `carrello_ibfk_2` FOREIGN KEY (`id_storage`) REFERENCES `storage` (`id_storage`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Limiti per la tabella `storage`
+--
+ALTER TABLE `storage`
+  ADD CONSTRAINT `storage_ibfk_1` FOREIGN KEY (`id_cap`) REFERENCES `cappelli` (`id_cap`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
