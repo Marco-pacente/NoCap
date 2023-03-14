92cfe3f0dff8b0469cec2b5226214ae8a69f7eeb
Marco-pacente <66440998+Marco-pacente@users.noreply.github.com>
2023-03-14 12:53:15 +0100

    Add files via upload

--- signup_redirect.php
+++ signup_redirect.php
@@ -3,6 +3,7 @@
         <link rel="stylesheet" href="style.css">
         <link href='https://fonts.googleapis.com/css?family=Monoton' rel='stylesheet'>
         <title>NoCap</title>
+        <script src="./scripts/signup.js"></script>
     </head>
     <body>
         <h1 class="title">NoCap?</h1>
@@ -16,11 +17,34 @@
             $email = trim($_POST["email"]); 
             $username = trim($_POST["username"]);
             $password = trim($_POST["password"]);
-            $bday = trim($_POST["bday"]);
+            $bday = $_POST["bday"];
             if(strlen($username)<4 || strlen($username)>20){
                 $isValid = false;
-                echo("Username non valido");
+                echo("Username non valido<br>");
             }
-            if(strlen($email))
+            $emailRegex = "/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g";
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                echo("Email non valida<br>");
+                $isValid = false;
+            }
+            if(strlen($password)<8){
+                echo("La password deve essere di almeno 8 caratteri<br>");
+                $isValid = false;
+            }
+            if($isValid){
+                $mysqli = new mysqli("localhost", "root", "", "cappelli");
+                if($mysqli -> connect_errno){
+                    echo $mysqli -> connect_error;
+                    exit();
+                }else{
+                    $inserimento = $mysqli->prepare("INSERT INTO users(username, password, email, birthdate) VALUES (?, ?, ?, ?)");
+                    $inserimento -> bind_param("sssd", $username, $password, $email, $bday);
+                    $inserimento -> execute();
+                }
+            }else{
+                echo '
+                    Se non vieni reindirizzato automaticamente, premi <a href="signup.php">qui</a>
+                ';
+            }
         ?>
     </body>
