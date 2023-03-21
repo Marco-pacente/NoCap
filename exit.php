61226af39353eaf3fc31343067270ee177f93335
Marco-pacente <66440998+Marco-pacente@users.noreply.github.com>
2023-03-21 10:28:43 +0100

    Update exit.php

--- exit.php
+++ exit.php
@@ -1,6 +1,6 @@
 <?php 
     session_start();
     session_destroy();
-    echo "<script> window.location.href = 'index.php'; </script>"
+    header("Location: index.php");
 ?>
 <p>Se non vieni reindirizzato automaticamente, premi <a href="account">qui</a></p>
